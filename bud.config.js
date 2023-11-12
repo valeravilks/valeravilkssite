14534c51b5170cdcf0d300fe8b9cc4470825667d
Valera-V <valera@dreamersofday.co>
2023-11-12 21:01:47 +0300

    Update config

--- bud.config.js
+++ bud.config.js
@@ -34,7 +34,7 @@ export default async (app) => {
    */
   app
     .setUrl('http://localhost:3000')
-    .setProxyUrl('http://example.test')
+    .setProxyUrl('https://my-first-wordpress-app.lndo.site')
     .watch(['resources/views', 'app']);
 
   /**
