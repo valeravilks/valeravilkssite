2fdc4993db89d54dbd498a97c818329722ba85cc
Valera-V <valera@dreamersofday.co>
2023-11-23 09:00:20 +0300

    Add Headers Atom in Design System Page

--- resources/views/design-system.blade.php
+++ resources/views/design-system.blade.php
@@ -11,4 +11,16 @@
   <x-atoms.docs-commets.index :class="App\View\Components\Atoms\Example::class"/>
 </div>
 
-
+<x-atoms.headers>
+  No Props
+</x-atoms.headers>
+<x-atoms.headers tag="2">
+  Tag 2
+</x-atoms.headers>
+<x-atoms.headers tag="2" styles="3">
+  Tag 2 Style 3
+</x-atoms.headers>
+<x-atoms.headers tag="2" styles="5">
+  Tag 2 Style 3
+</x-atoms.headers>
+<x-atoms.docs-commets.index :class="App\View\Components\Atoms\Headers::class"/>
