19d119a6dfa9fc1b9f7f4e9cc0c68bbf12385e40
Valera-V <valera@dreamersofday.co>
2023-12-06 08:49:17 +0300

    Add Example Atom docs add $classComponent variable

--- app/View/Components/Atoms/Example.php
+++ app/View/Components/Atoms/Example.php
@@ -11,6 +11,19 @@ use Illuminate\View\Component;
 class Example extends Component
 {
     /**
+     * Required element is the name of this component. Use this title format:
+     *
+     * a - denotes an atom
+     * example - class name with small letters.
+     *
+     * If your atom is in some subfolder, indicate the path using the '-' sign.
+     * For example: 'a-headers-title', this means that this file is located in
+     * the App\View\Components\Atoms\Headers\Title folder
+     */
+
+    public string $classComponent = 'a-example';
+
+    /**
      * Create a new component instance.
      *
      * @return void
