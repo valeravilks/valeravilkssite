6a2a058d5d2762c86891f8aaec982b80d0f61bd4
Valera-V <valera@dreamersofday.co>
2023-12-11 08:59:00 +0300

    Add color in config

--- resources/views/components/atoms/example.blade.php
+++ resources/views/components/atoms/example.blade.php
@@ -1,3 +1,4 @@
 <div @class([$classComponent, 'active'])>
     This is Example Atom Component
+  <div class="w-4 h-4 bg-primary"></div>
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,16 @@ const config = {
   content: ['./index.php', './app/**/*.php', './resources/**/*.{php,vue,js}'],
   theme: {
     extend: {
-      colors: {}, // Extend Tailwind's default colors
+      colors: {
+        primary: '#0003ff',
+        secondary: '#C90000',
+        white: '#ffffff',
+        black: '#000000',
+        light: '#F6F5F5',
+        dark: '#361515',
+        gray: '#D9D9D9',
+        lightgray: '#BBBBBB',
+      }, // Extend Tailwind's default colors
     },
   },
   plugins: [],
